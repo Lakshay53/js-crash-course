cbdb086bba9fa9bf87d5c069a866e5cbdb192c77
lakshay53 <1818005@gmail.com>
2022-08-16 13:41:16 -0400

    created multiple users

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-let arr = [1, 5, 10, 3]
-
-let newArray = arr.map(element => element * 100)
-
-console.log(newArray)
-
-let arrCents = []
-
-for (let i = 0; i < arr.length; ++i) {
-    arrCents.push(arr[i] * 100)
+let user = [{
+    username: 'Lakshay53',
+    email: 'contact@lakshaysharma.xyz',
+    password: 'test123',
+    subscriptionStatus: 'VIP',
+    discordId: 'Iann Dior#1230',
+    lessonsCompleted: [0, 1]
+},
+{
+    username: 'Maddy',
+    email: 'maddy@lakshaysharma.xyz',
+    password: 'test1234',
+    subscriptionStatus: 'VIP',
+    discordId: 'Kenji#9592',
+    lessonsCompleted: [0, 1, 2, 3]
 }
 
-console.log(arrCents)
+]
+
+function login(email, password){
+    for (let i = 0; i < user.length; ++i){
+        if (user[i].email === email){
+            if (user[i].password === password){
+                console.log(user[i])
+                console.log('the details are correct - you are now logged in')
+            }
+            else{
+                console.log('not correct password')
+            }
+        }
+    }
+}
+
+login('maddy@lakshaysharma.xyz', 'test1234')
