bad78695b2aa4f0929140bab5e3172fd8214667c
lakshay53 <1818005@gmail.com>
2022-08-16 14:01:23 -0400

    push new users if necesarry

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
-let user = [{
-    username: 'Lakshay53',
-    email: 'contact@lakshaysharma.xyz',
-    password: 'test123',
-    subscriptionStatus: 'VIP',
-    discordId: 'Iann Dior#1230',
-    lessonsCompleted: [0, 1]
-},
-{
-    username: 'Maddy',
-    email: 'maddy@lakshaysharma.xyz',
-    password: 'test1234',
-    subscriptionStatus: 'VIP',
-    discordId: 'Kenji#9592',
-    lessonsCompleted: [0, 1, 2, 3]
-}
+let users = [
+  {
+    username: "Lakshay53",
+    email: "contact@lakshaysharma.xyz",
+    password: "test123",
+    subscriptionStatus: "VIP",
+    discordId: "Iann Dior#1230",
+    lessonsCompleted: [0, 1],
+  },
+  {
+    username: "Maddy",
+    email: "maddy@lakshaysharma.xyz",
+    password: "test1234",
+    subscriptionStatus: "VIP",
+    discordId: "Kenji#9592",
+    lessonsCompleted: [0, 1, 2, 3],
+  },
+];
 
-]
-
-function login(email, password){
-    for (let i = 0; i < user.length; ++i){
-        if (user[i].email === email){
-            if (user[i].password === password){
-                console.log(user[i])
-                console.log('the details are correct - you are now logged in')
-            }
-            else{
-                console.log('not correct password')
-            }
-        }
+function login(email, password) {
+  for (let i = 0; i < users.length; ++i) {
+    if (users[i].email === email) {
+      if (users[i].password === password) {
+        console.log(users[i]);
+        console.log("the details are correct - you are now logged in");
+      } else {
+        console.log("not correct password");
+      }
+      return;
     }
+  }
+  console.log("couldn't find a email that matches");
 }
 
-login('maddy@lakshaysharma.xyz', 'test1234')
+login("maddy@lakshaysharma.xyz", "test1234");
+
+function register(user){
+    users.push(user)
+}
+
+register({
+    username: 'Sia',
+    email: 'Sia@lakshaysharma.xyz',
+    password: 'sia123',
+    subscriptionStatus: 'VIP',
+    discordId: 'Sia#0001',
+    lessonsCompleted: [0,1],
+});
+
+console.log(users)
